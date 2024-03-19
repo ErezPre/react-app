c372cfe9cf943cf5b35eed6a3e610b2f954f080a
Erez Prepelitzki <prepelitzki@gmail.com>
2024-03-19 04:26:52 +0200

    single cocktail destructure works

--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -3,23 +3,67 @@ import { useParams } from "react-router-dom";
 
 function Cocktail() {
   const { id } = useParams();
-  const [cocktail, setCocktail] = useState();
+  const [cocktail, setCocktail] = useState(null);
+
   useEffect(() => {
-    const selectedCocktail = fetch(
-      `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-    )
-      .then((response) => response.json())
-      .then((response) => response.drinks[0])
-      .catch((error) => {
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        console.log("id is", id);
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+          console.log(newCocktail.name);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
         console.log(error);
-      });
-    const { strDrink } = selectedCocktail;
+      }
+    }
+    getCocktail();
   }, [id]);
-  return (
-    <>
-      <h1 className="cocktail page">Cocktail {id}</h1>
-    </>
-  );
+
+  if (cocktail) {
+    const { name, image, category, info, glass, instructions, ingredients } =
+      cocktail;
+    return (
+      <>
+        <h1 className="cocktail page">Cocktail {name}</h1>
+      </>
+    );
+  }
 }
 
 export default Cocktail;
